41f42fb39152ce753f98e9c75aa3ea24ad23b4a6
Gökhan İpek <gipek03@gmail.com>
2018-04-17 09:26:16 +0300

    fake server data
    added data for playlists

--- src/App.js
+++ src/App.js
@@ -17,17 +17,17 @@ let fakeServerData = {
       },
       {
         name: 'Discover Weekly',
-        songs: [{name: 'Beat it', duration: 1166}, {name: 'makaroni', duration: 1423}, {name: 'rockstar', duration: 2353 }]
+        songs: [{name: 'Evidence', duration: 1166}, {name: 'Chrome', duration: 1423}, {name: 'For My Demons', duration: 2353 }]
       },
       {
         name: 'Rap Music',
-        songs: [{name: 'Beat it', duration: 1166}, {name: 'makaroni', duration: 1423}, {name: 'rockstar', duration: 1111 }]
+        songs: [{name: 'Pool of Booze', duration: 1166}, {name: 'Pearl Lady', duration: 1423}, {name: 'For Evight', duration: 1111 }]
       }
     ]
   }
 }
 
-class PlaylistCounterAggragate extends Component {
+class PlaylistCounter extends Component {
   render(){
     return(
       <div style={{display: 'inline-block', width: '40%'}} >
@@ -39,7 +39,7 @@ class PlaylistCounterAggragate extends Component {
 }
 
 
-class HoursCounterAggragate extends Component {
+class HoursCounter extends Component {
   render(){
     let allSongs = this.props.playlists.reduce((songs, eachPlaylist)=> {
       return eachPlaylist.songs.concat(eachPlaylist.songs)
@@ -70,14 +70,15 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render(){
+    let playlist = this.props.playlist
     return(
       <div style={{...defaultStyle, display: 'inline-block', width: '20%'}}>
         <img/>
-        <h3>Playlist Name</h3>
+        <h3>{playlist.name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>  
+          {playlist.songs.map(song => 
+            <li>{song.name}</li>
+          )}
         </ul>
 
         </div>
@@ -97,6 +98,15 @@ class App extends Component {
 
   }
   render() {
+    let playlistElements = [];
+    if(this.state.serverData.user){
+      for (let i = 0; i <this.state.serverData.user.playlists.length; i++) {
+        let playlist = this.state.serverData.user.playlists[i]
+        playlistElements.push(<Playlist playlist= {playlist}/>)
+        
+      }
+    }
+    this.state.serverData.playlists
     let name = 'Gokhan'
     let green  ='#4CE0D2'
     let fullLength = '100%'
@@ -107,17 +117,18 @@ class App extends Component {
           <div>
           <h1 style={{ ...defaultStyle, 'font-size': '54px'}}>
             {
-              this.state.serverData.user.name
-            } First App
-          </h1>
-        }
-        <PlaylistCounterAggragate playlists={this.state.serverData.user.playlists }/>
-        <HoursCounterAggragate playlists={this.state.serverData.user.playlists }/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        </div> : <div style= {{...defaultStyle}}>loading...</div>
+                this.state.serverData.user.name
+              } First App
+            </h1>
+          }
+          <PlaylistCounter playlists={this.state.serverData.user.playlists }/>
+          <HoursCounter playlists={this.state.serverData.user.playlists }/>
+          <Filter/>
+          {this.state.serverData.user.playlists.map(playlist => 
+            <Playlist playlist={playlist} />
+            )}
+          
+          </div> : <h1 style= {{...defaultStyle}}>loading...</h1>
         }
       </div>
     );
