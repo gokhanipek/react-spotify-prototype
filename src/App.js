d7ce40967f06938afb871cc771b63a66f6a9078e
Gökhan İpek <gipek03@gmail.com>
2018-05-02 13:25:57 +0300

    filter-songs
    filter applies to songs too

--- src/App.js
+++ src/App.js
@@ -150,10 +150,13 @@ class App extends Component {
     let playlistToRender = 
     this.state &&
      this.state.playlists 
-      ? this.state.playlists.filter(playlist =>
-          playlist.name.toLowerCase().includes(
-            this.state.filterString.toLowerCase())) 
-          : []
+      ? this.state.playlists.filter(playlist => {
+        let matchesPlaylist = playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase()) 
+          let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
+          .includes(this.state.filterString.toLowerCase()))
+        return matchesPlaylist ||matchesSong
+        }) : []
     return (  
       <div className="App">
         {this.state.user ?
@@ -174,7 +177,7 @@ class App extends Component {
            : <button onClick={() => {
               window.location = window.location.href.includes('localhost') ? 'http://localhost:8888/login' : 'https://react-spotify-backend.herokuapp.com/login'}
               }
-            style={{'padding': '20px', 'margin-top': '50px'}}>Sign in with Spotify</button>
+            style={{'padding': '20px', 'marginTop': '50px'}}>Sign in with Spotify</button>
         }
       </div>
     );
